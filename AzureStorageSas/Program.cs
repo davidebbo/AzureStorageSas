3128d079bde2343895afac18d4572688d7485773
davidebbo <david.ebbo@microsoft.com>
2013-07-01 18:29:39 -0700

    Initial commit

---
+++ AzureStorageSas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorageSas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.AppSettings["STORAGE_CONNECTION_STRING"];
+                string containerName = ConfigurationManager.AppSettings["CONTAINER_NAME"];
+
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                // Retrieve a reference to a container. 
+                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+                var sas = container.GetSharedAccessSignature(new SharedAccessBlobPolicy()
+                {
+                    Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.List | SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.Delete,
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddYears(1000)
+                });
+
+                Console.WriteLine(container.Uri + sas);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
